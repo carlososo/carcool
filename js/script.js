dcac2e7f4193a81dc7e03f3307fb2cea3c4c848c
Abel Mendiola <abel.mendiola@comunidad.unam.mx>
2020-07-15 00:57:34 -0500

    Codigo cotizador

--- js/script.js
+++ js/script.js
@@ -7,6 +7,7 @@
     let precio = []
     let colores = []
     let transmision = []
+    let presupuesto = []
     for (let i = 0; i < autos.length; i++) {
         año.push(autos[i].year);
         marca.push(autos[i].make);
@@ -15,6 +16,8 @@
         precio.push(autos[i].price);
         colores.push(autos[i].colors);
         transmision.push(autos[i].transmision);
+        // Calculo del valor maximo de enganche
+        presupuesto.push(autos[i].price * 0.2)
     }
     console.log(año);
     console.log(marca);
@@ -23,5 +26,32 @@
     console.log(precio);
     console.log(colores);
     console.log(transmision);
+    console.log(presupuesto)
+
+    let card = []
+
+    // funcion para busqueda de presupuesto
+    buscar.addEventListener('click', function() {
+        let busqueda = document.getElementById('busqueda').value;
+        for (var i = 0; i < autos.length; i++) {
+            var budget = Math.floor(autos[i].price * 0.2)
+            if ( budget <= busqueda) {
+                card.push(
+                    autos[i].make
+                    autos[i].model
+                    autos[i].colors
+                    autos[i].price
+                )
+                busquedaCar.push(car[p])
+                insertarCar(car[p]);
+            }
+        }
+
+
+
+
+
+
+
 
 })();
