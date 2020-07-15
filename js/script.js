6d207b6ee558204f656859c161dada7e061c34f3
Carlos <carlosoc@live.com>
2020-07-14 21:31:58 -0500

    primer commit a repo remoto

---
+++ js/script.js
@@ -0,0 +1,27 @@
+(function() {
+    'use strict'
+    let año = [];
+    let marca = [];
+    let modelo = [];
+    let tipo = []
+    let precio = []
+    let colores = []
+    let transmision = []
+    for (let i = 0; i < autos.length; i++) {
+        año.push(autos[i].year);
+        marca.push(autos[i].make);
+        modelo.push(autos[i].model);
+        tipo.push(autos[i].body_styles);
+        precio.push(autos[i].price);
+        colores.push(autos[i].colors);
+        transmision.push(autos[i].transmision);
+    }
+    console.log(año);
+    console.log(marca);
+    console.log(modelo);
+    console.log(tipo);
+    console.log(precio);
+    console.log(colores);
+    console.log(transmision);
+
+})();
