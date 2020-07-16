e40560b344ceb7e57603b01e8c276ae533817b6e
amendiolar <abel.mendiola@comunidad.unam.mx>
2020-07-16 15:02:51 -0500

    Propuesta para mostrar cartas con base en la informacion ingresada por el usuario en el cotizador

--- js/script.js
+++ js/script.js
@@ -28,12 +28,48 @@
         // Obtener la informacion del cotizador
 
         // Marca MAKE
+    var manufacturedBy = " "
     var company = document.getElementById(marca).value
     if (company == "0"){
-        var manufacturedBy= "Chevrolet"
+        manufacturedBy= "Chevrolet"
     } else if (company == "1"){
-        var manufacturedBy = "Ford"
+        manufacturedBy = "Ford"
     } else { manufacturedBy = "Nissan"}
+
+    // Obtener el resto de la informacion del cotizador y procesarla
+    var modelo = document.getElementById(anio).value
+    var engranaje = document.getElementById(transmision)
+    var budget = document.getElementById(presupuesto).value 
+
+    // Buscar opciones en la base de datos
+    for(var i=0; i<=autos.length; i++){
+        var aGastar = autos[i].price * 0.2
+        if (autos[i].year == modelo && autos[i].make == manufacturedBy 
+            && aGastar <= budget && autos[i].transmision == engranaje) {
+                let cardContainer = document.getElementById('card')
+                let carta = document.createElement('div')
+                carta.classList.add('col-xl-3')
+                carta.classList.add('col-lg-4')
+                carta.classList.add('col-md-6')
+                carta.classList.add('justify-content-between')
+                carta.setAttribute('id',elementos.id)
+                carta.innerHTML = '<div class="card borde my-4">
+                                <figure class="borde fondo-imagen">
+                                <img src=""></img>
+                                </figure>
+                                <div class="card-body">
+                                    <p>${autos[i].make}</p>
+                                    <p>${autos[i].year}</p>
+                                    <p>${autos[i].colors}</p>
+                                    <p>${autos[i].price}</p>
+                                </div>
+                            </div>'
+
+            } else {}
+        
+    }                                                                                                                                                
+
+
 
 
 
