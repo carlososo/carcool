9c94a61cc5df2c9c2c3115d18574ccdb9739cf4e
Carlos <carlosoc@live.com>
2020-07-16 21:51:40 -0500

    modificaciones al script.js e index.html funcionando

--- js/script.js
+++ js/script.js
@@ -1,15 +1,52 @@
 (function() {
     'use strict'
-
-
+    let cardContainer = document.getElementById('linea')
+    console.log(cardContainer);
     let boton = document.getElementById('boton');
     boton.addEventListener('click', function() {
         let marcaUsuario = document.getElementById('marca').value;
         let anioUsuario = document.getElementById('anio').value;
         let transmisionUsuario = document.getElementById('transmision').value;
-        console.log(traductorMarca(marcaUsuario));
-        console.log(traductorAnio(anioUsuario));
-        console.log(traductorTransmision(transmisionUsuario));
+        var budget = document.getElementById('presupuesto').value
+        budget = parseInt(budget);
+
+
+        for (var i = 0; i < autos.length; i++) {
+            // console.log(autos[i].price);
+            var aGastar = autos[i].price * 0.2;
+            console.log(autos[i].year);
+            if (autos[i].year == traductorAnio(anioUsuario) && autos[i].make == traductorMarca(marcaUsuario) &&
+                aGastar <= budget && autos[i].transmision == traductorTransmision(transmisionUsuario)) {
+                let cardContainer = document.getElementById('linea')
+                let carta = document.createElement('div')
+                carta.classList.add('col-xl-3')
+                carta.classList.add('col-lg-4')
+                carta.classList.add('col-md-6')
+                carta.classList.add('justify-content-between')
+                    // carta.setAttribute('id', elementos.id)
+                carta.innerHTML = `<div class="card borde my-4">
+                                    <figure class="borde fondo-imagen">
+                                    <img src="img/fog-of-war-3440x1440jpg.jpg" alt="">
+                                    </figure>
+                                    <div class="card-body">
+                                        <p>${autos[i].model}</p>
+                                        <p>${autos[i].make}</p>
+                                        <p>${autos[i].year}</p>
+                                        <p>${autos[i].colors}</p>
+                                        <p><span class="icono" ><i class="fas fa-dollar-sign"></i> </span> ${autos[i].price}</p>
+                                    </div>
+                                </div>`
+
+                cardContainer.appendChild(carta);
+
+            } else {}
+
+        }
+
+        // console.log(traductorMarca(marcaUsuario));
+        // console.log(traductorAnio(anioUsuario));
+        // console.log(traductorTransmision(transmisionUsuario));
+        // console.log(budget);
 
     });
 
@@ -40,78 +77,12 @@
             return "auto";
     }
 
+    // Obtener la informacion del cotizador
 
-
-    let año = [];
-    let marca = [];
-    let modelo = [];
-    let tipo = []
-    let precio = []
-    let colores = []
-    let transmision = []
-    for (let i = 0; i < autos.length; i++) {
-        año.push(autos[i].year);
-        marca.push(autos[i].make);
-        modelo.push(autos[i].model);
-        tipo.push(autos[i].body_styles);
-        precio.push(autos[i].price);
-        colores.push(autos[i].colors);
-        transmision.push(autos[i].transmision);
-    }
-    // console.log(año);
-    // console.log(marca);
-    // console.log(modelo);
-    // console.log(tipo);
-    // console.log(precio);
-    // console.log(colores);
-    // console.log(transmision);
-
-
-
-
-        // Obtener la informacion del cotizador
-
-        // Marca MAKE
-    var manufacturedBy = " "
-    var company = document.getElementById(marca).value
-    if (company == "0"){
-        manufacturedBy= "Chevrolet"
-    } else if (company == "1"){
-        manufacturedBy = "Ford"
-    } else { manufacturedBy = "Nissan"}
-
-    // Obtener el resto de la informacion del cotizador y procesarla
-    var modelo = document.getElementById(anio).value
-    var engranaje = document.getElementById(transmision)
-    var budget = document.getElementById(presupuesto).value 
 
     // Buscar opciones en la base de datos
-    for(var i=0; i<=autos.length; i++){
-        var aGastar = autos[i].price * 0.2
-        if (autos[i].year == modelo && autos[i].make == manufacturedBy 
-            && aGastar <= budget && autos[i].transmision == engranaje) {
-                let cardContainer = document.getElementById('card')
-                let carta = document.createElement('div')
-                carta.classList.add('col-xl-3')
-                carta.classList.add('col-lg-4')
-                carta.classList.add('col-md-6')
-                carta.classList.add('justify-content-between')
-                carta.setAttribute('id',elementos.id)
-                carta.innerHTML = '<div class="card borde my-4">
-                                <figure class="borde fondo-imagen">
-                                <img src=""></img>
-                                </figure>
-                                <div class="card-body">
-                                    <p>${autos[i].make}</p>
-                                    <p>${autos[i].year}</p>
-                                    <p>${autos[i].colors}</p>
-                                    <p>${autos[i].price}</p>
-                                </div>
-                            </div>'
 
-            } else {}
-        
-    }                                                                                                                                                
+
 
 
 
@@ -121,3 +92,37 @@
 
 
 })();
+
+
+
+
+// let año = [];
+// let marca = [];
+// let modelo = [];
+// let tipo = []
+// let precio = []
+// let colores = []
+// let transmision = []
+// for (let i = 0; i < autos.length; i++) {
+//     año.push(autos[i].year);
+//     marca.push(autos[i].make);
+//     modelo.push(autos[i].model);
+//     tipo.push(autos[i].body_styles);
+//     precio.push(autos[i].price);
+//     colores.push(autos[i].colors);
+//     transmision.push(autos[i].transmision);
+
+// var manufacturedBy = " "
+// var company = document.getElementById(marca).value
+// if (company == "0") {
+//     manufacturedBy = "Chevrolet"
+// } else if (company == "1") {
+//     manufacturedBy = "Ford"
+// } else { manufacturedBy = "Nissan" }
+
+// Marca MAKE
+
+
+// Obtener el resto de la informacion del cotizador y procesarla
+// var modelo = document.getElementById('anio').value
+// var engranaje = document.getElementById('transmision')
