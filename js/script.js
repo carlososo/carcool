31e551b7e5aead4b541d95c5674a88c301733903
Carlos <carlosoc@live.com>
2020-07-16 19:15:20 -0500

    creacion de rama, adicion de boton y variables para capturar los valores

--- js/script.js
+++ js/script.js
@@ -1,5 +1,47 @@
 (function() {
     'use strict'
+
+
+    let boton = document.getElementById('boton');
+    boton.addEventListener('click', function() {
+        let marcaUsuario = document.getElementById('marca').value;
+        let anioUsuario = document.getElementById('anio').value;
+        let transmisionUsuario = document.getElementById('transmision').value;
+        console.log(traductorMarca(marcaUsuario));
+        console.log(traductorAnio(anioUsuario));
+        console.log(traductorTransmision(transmisionUsuario));
+
+    });
+
+
+    function traductorMarca(valor) {
+
+        if (valor == 1) {
+            return "Chevrolet";
+        } else if (valor == 2) {
+            return "Ford";
+        } else {
+            return "Nissan";
+        }
+
+    }
+
+    function traductorAnio(valor) {
+        if (valor == 1) {
+            return 2019;
+        } else
+            return 2020;
+    }
+
+    function traductorTransmision(valor) {
+        if (valor == 1) {
+            return "manual"
+        } else
+            return "auto";
+    }
+
+
+
     let año = [];
     let marca = [];
     let modelo = [];
@@ -16,12 +58,14 @@
         colores.push(autos[i].colors);
         transmision.push(autos[i].transmision);
     }
-    console.log(año);
-    console.log(marca);
-    console.log(modelo);
-    console.log(tipo);
-    console.log(precio);
-    console.log(colores);
-    console.log(transmision);
+    // console.log(año);
+    // console.log(marca);
+    // console.log(modelo);
+    // console.log(tipo);
+    // console.log(precio);
+    // console.log(colores);
+    // console.log(transmision);
+
+
 
 })();
